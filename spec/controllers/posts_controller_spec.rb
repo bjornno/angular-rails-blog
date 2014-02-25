0f08a128358aa5a6041b1507698758e38c559a9b
Bjørn Nordlund <bnordlun@cisco.com>
2014-02-25 15:49:39 +0100

    added rails controller spec

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe PostsController do
+  describe "GET index" do
+    it "renders posts as json" do
+    	post_data = {title: "foo", contents: "bar"}
+      team = Post.create post_data
+      get :index, :format => :json
+      response.should be_success
+      parsed_body = JSON.parse(response.body)
+      parsed_body.length.should == 1
+      parsed_body.first["title"].should == "foo" 
+    end
+  end
+
+end
