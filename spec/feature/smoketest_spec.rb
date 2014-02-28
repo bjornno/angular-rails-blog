52cc8aee17c56697981d3101da84716a8752b708
Bjørn Nordlund <bnordlun@cisco.com>
2014-02-28 09:48:08 +0100

    fixed smoketest to work with links

--- spec/feature/smoketest_spec.rb
+++ spec/feature/smoketest_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 Capybara.default_driver = :selenium
+Capybara.default_wait_time = 10
+
 describe 'smoketest' do
-#	before { pending "Excluded running features as they run in browser"}
+	before { pending "Excluded running features as they run in browser"}
 
   it 'creates a blog post' do
     visit '/'
 		page.should have_title('Blog')
-    click_on 'new post'
+    find("#new_post").click
     expect(page).to have_content 'Create new post'
     fill_in 'title', with: 'blog title'
     fill_in 'contents', with: 'blog contents'
@@ -15,7 +17,7 @@ describe 'smoketest' do
     expect(page).to have_content 'Posts'
     expect(page).to have_content 'blog title'
     expect(page).to have_content 'blog contents'
-    click_on 'blog title'
+    find('a', :text => 'blog title').click
     click_on 'Delete'
     expect(page).to have_no_content 'blog title'
     
