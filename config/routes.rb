8be6c3b765254d3f1d495577fa1d1e0136e6ca92
Bjørn Nordlund <bnordlun@cisco.com>
2014-03-03 12:21:47 +0100

    adding user facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Blog::Application.routes.draw do
 
   resources :posts
 
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
